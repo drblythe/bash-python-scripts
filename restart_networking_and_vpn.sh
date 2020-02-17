94e7ffa61969c4e699ce5392edb26639b376c252
Michael Graham <gra3@pm.me>
2020-02-17 11:15:42 -0600

    Added network/vpn restart script

--- restart_networking_and_vpn.sh
+++ restart_networking_and_vpn.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo systemctl stop network-online.target
-sudo systemctl stop NetworkManager
-sudo systemctl stop windscribe.service
-sudo systemctl start network-online.target
-sudo systemctl start NetworkManager
-sudo systemctl start windscribe.service
+
+echo "Disconnecting from Windscribe..."
+windscribe disconnect
+echo "\"Restarting\" services..."
+sudo systemctl stop windscribe.service network-online.target NetworkManager
+sudo systemctl start windscribe.service network-online.target NetworkManager
+echo "Connecting to Windscribe (BBQ)..."
+windscribe connect bbq
+
